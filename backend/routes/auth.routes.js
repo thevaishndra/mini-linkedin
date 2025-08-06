de14348c355c68bc3aa8b31b6d941683cc03d2cc
thevaishndra <chandrav306m@gmail.com>
2025-08-06 20:05:11 +0530

    backend error resolved

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { signup, login, logout, updateProfile, checkAuth } from '../controllers/auth.controller';
-import { protectRoute } from '../middlewares/auth.middleware';
+import { signup, login, logout, updateProfile, checkAuth } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { createPost, getAllPublicPosts, getUserPosts } from '../controllers/post.controller';
-import { protectRoute } from "../middlewares/auth.middleware";
+import { createPost, getAllPublicPosts, getUserPosts } from '../controllers/post.controller.js';
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
